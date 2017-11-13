0656d7f0fdb612695f2ce0ef984a5ddebbc4fc6c
Charles <cweng@stuy.edu>
2017-11-13 03:52:38 -0500

    testing cont

--- args.c
+++ args.c
@@ -19,13 +19,19 @@
    char ** args = calloc(5, sizeof(char *));
    int i = 0;
    // while(line){
-     args[i] = (char *) strsep(&line, " ");
+     // args[i] = (char *) strsep(&line, " ");
    //   i++;
    // }
    return args;
  }
 
 int main() {
+  // testing
+  char line[100] = "wow-this-is-cool";
+  char *s1 = line;
+  printf("[%s]\n", strsep( &s1, "-" ));
+  printf("[%s]\n", s1);
+
   char ** x = (char **) parse_args("ls -a -l");
   printf("%s %s %s   \n", x[0], x[1], x[2]);
   // execvp(x[0], x);
